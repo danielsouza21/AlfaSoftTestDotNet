a36fef3df245848f61cc60bb89cd4f6d9c91a60d
Daniel Oliveira Souza <C0640020@vale.com>
2022-04-24 11:49:18 -0300

    Read lines from txt file input

--- AlfaSoftTest/Program.cs
+++ AlfaSoftTest/Program.cs
@@ -1,26 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
 namespace AlfaSoftTest
 {
-    public static class Program
+    public class Program
     {
         private const string FILE_INPUT_EXTENSION = ".txt";
 
-        static void Main(string[] args)
+        public void Main(string[] args)
         {
-            //TODO: Extrair para método e facade
             if (args is null || args.Count() != 1) throw new Exception("Invalid entries. Correct execution must have a single argument with full path to a txt file.");
 
+            var linesFromTxtInput = GetLinesFromTxtFile(args);
+
+        }
+
+        public List<string> GetLinesFromTxtFile(string[] args)
+        {
+            //TODO: Extrair para facade de acesso a arquivos
             var filePath = args[0];
-            if(!File.Exists(filePath)) throw new Exception($"File not exists {filePath}");
+            if (!File.Exists(filePath)) throw new Exception($"File not exists {filePath}");
 
             var fileType = Path.GetExtension(filePath);
             if (fileType != FILE_INPUT_EXTENSION) throw new Exception($"File should be {FILE_INPUT_EXTENSION}");
 
-            var lines = File.ReadLines(filePath).ToList();
-            lines.ForEach(x => Console.WriteLine(x));
+            return File.ReadLines(filePath).ToList();
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # AlfaSoftTestDotNet
+
+dotnet run C:\Users\dti Digital\source\repos\AlfaSoftTest\AlfaSoftTest\Users.txt
